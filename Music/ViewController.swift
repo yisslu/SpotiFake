26b168e51ecd0983dce34e9cd7c9e1f6d78efc14
yisslu <lugohalo999@gmail.com>
2024-10-18 14:22:47 -0600

    Initial Commit

---
+++ Music/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Diplomado on 18/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
